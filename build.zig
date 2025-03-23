cf7e8faf4d08dfe071451fa357cc7f7d91ca5354
Meghan Denny <hello@nektro.net>
2025-03-23 03:02:35 -0700

    remove exe

--- build.zig
+++ build.zig
@@ -6,26 +6,6 @@ pub fn build(b: *std.Build) !void {
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
     const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
-    const exe = b.addExecutable(.{
-        .name = "koino",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = mode,
-    });
-    deps.addAllTo(exe);
-    exe.use_llvm = !disable_llvm;
-    exe.use_lld = !disable_llvm;
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
     const test_exe = b.addTest(.{
         .name = "test",
         .root_source_file = b.path("src/main.zig"),
