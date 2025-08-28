12803a820778693fab8d0157314aaf454f6f1797
Meghan Denny <hello@nektro.net>
2025-08-28 12:16:15 -0700

    build.zig: ensure tests are run with project root as cwd

--- build.zig
+++ build.zig
@@ -18,6 +18,7 @@ pub fn build(b: *std.Build) !void {
 
     const test_step = b.step("test", "Run all the tests");
     const test_run = b.addRunArtifact(test_exe);
+    test_run.setCwd(b.path("."));
     test_run.has_side_effects = true;
     test_step.dependOn(&test_run.step);
 }
